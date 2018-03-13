da9ad85f2f5cda800f61507409a6254b8c0ccec8
Ari Vidal <ariel.m.vidal@gmail.com>
2018-03-13 13:39:56 -0400

    commiting files

---
+++ assets/php/contact.php
@@ -0,0 +1,35 @@
+<!-- <?php
+
+// if(isset($_POST['message'])){
+
+// 	$name = $_POST['name'];
+// 	$email = $_POST['email'];
+// 	$message = $_POST['message'];
+    
+	
+// 	$to      = 'plawansusu@hotmail.com';
+// 	$subject = 'Portfolio contact';
+
+// 	$headers = 'From: '. $email . "\r\n" .
+//     'Reply-To: '. $email . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+
+// 	$status = mail($to, $subject, $message, $headers);
+
+// 	if($status == TRUE){	
+// 		$res['sendstatus'] = 'done';
+	
+// 		//Edit your message here
+// 		$res['message'] = 'Form Submission Successful';
+//     }
+// 	else{
+// 		$res['message'] = 'Form Submission Successful';
+// 	}
+	
+	
+// 	echo json_encode($res);
+}
+
+?>
+
+ -->
